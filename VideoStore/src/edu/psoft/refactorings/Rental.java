f28b627b8ff3927226a4c2c1175970e2260faa31
evertonUFCG <39099565+evertonUFCG@users.noreply.github.com>
2022-02-02 14:46:22 -0300

    Add files via upload

---
+++ VideoStore/src/edu/psoft/refactorings/Rental.java
@@ -0,0 +1,20 @@
+package edu.psoft.refactorings;
+
+public class Rental {
+
+	private Movie movie;
+	private int daysRented;
+
+	public Rental(Movie movie, int daysRented) {
+		this.movie = movie;
+		this.daysRented = daysRented;
+	}
+
+	public int getDaysRented() {
+		return daysRented;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+}
---
+++ VideoStore/src/edu/psoft/refactorings/tests/Client.java
@@ -0,0 +1,32 @@
+package edu.psoft.refactorings.tests;
+
+import edu.psoft.refactorings.Customer;
+import edu.psoft.refactorings.Movie;
+import edu.psoft.refactorings.Rental;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Customer customer = new Customer("Everton");
+		customer.addRental(new Rental(new Movie("Star Wars", 0), 3));
+		customer.addRental(new Rental(new Movie("Frozen", 2), 3));
+		customer.addRental(new Rental(new Movie("Matrix Resurrections", 1), 3));
+		System.out.println(customer.statement());
+		
+		System.out.println("-------------------");
+		Customer customer2 = new Customer("José");
+		customer2.addRental(new Rental(new Movie("The Godfather", 0), 3));
+		customer2.addRental(new Rental(new Movie("Matrix Resurrections", 1), 3));
+		System.out.println(customer2.statement());
+		
+		System.out.println("-------------------");
+		Customer customer3 = new Customer("Maria");
+		customer3.addRental(new Rental(new Movie("Moana", 2), 7));
+		customer3.addRental(new Rental(new Movie("Frozen", 2), 7));
+		customer3.addRental(new Rental(new Movie("The Lion King", 2), 7));
+		customer3.addRental(new Rental(new Movie("Aladin", 2), 7));
+		System.out.println(customer3.statement());
+
+	}
+
+}
